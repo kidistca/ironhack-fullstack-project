126992e3f0f59dfddc809e40ab8ce1f9a46789c2
kidistca <kidistca@gmail.com>
2019-09-18 17:02:38 +0100

    Order page created, filtered out selected item along with contact info

--- fullstack-project/routes/productRoute.js
+++ fullstack-project/routes/productRoute.js
@@ -1,36 +1,74 @@
 'use strict';
 
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const router = Router();
 
 const Image = require('../models/image');
-
+const Seller = require('./../models/seller');
 const checkBuyerLogin = require('../controller/checkBuyerLogin');
 
 
 
-router.get('/product', checkBuyerLogin,(req, res, next) => { 
-    console.log('Trying to get products'); 
-      Image.find({})
-        // .limit(20)
-        // .sort({ createdAt: -1 })
-        // .exec()
-      .then(products => {
-        // console.log("This are products", products);
-        res.render('product', { products });
-      })
-  
-      .catch(error => next(error));
-  });
-  
-router.get('/order', (req, res, next) => {
-  // console.log(req.session.uid);
-    res.render('order');
+router.get('/product', checkBuyerLogin, (req, res, next) => {
+  console.log('Trying to get products');
+  Image.find({})
+    // .limit(20)
+    // .sort({ createdAt: -1 })
+    // .exec()
+    .then(products => {
+      // console.log("This are products", products);
+      res.render('product', {
+        products
+      });
+    })
 
-});  
+    .catch(error => next(error));
+});
 
 
 
+
+// router.get('/product/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   // const sellerId = Seller._id;
+//   Promise.all([Image.findById(id), Seller.findById("sellerId")])
+//      .then(result => {
+//        const data = {
+//          ImageResult: result[0],
+//          sellerResult: result[1]
+//        };
+//      res.render('order', data);
+//   })
+//   .catch(error => next(error));
+// });
+
+
+
+router.get('/product/:id', (req, res, next) => {
+  const productId = req.params.id;
+  Image.findById(productId)
+    .then(oneProduct => {
+      const sellerId = oneProduct.sellerId;
+      Seller.findById(sellerId)
+        .then(oneSeller => {
+          console.log("the seller's name", oneSeller.name);
+          // res.render('order',{oneSeller});
+          res.render('order', {
+            oneProduct,
+            oneSeller
+          });
+
+        });
+    })
+    .catch(error => next(error));
+});
+
+
+router.get('/ordersuccessful', (req, res, next) => {
+  res.render('orderSuccessful');
+});
 
 
 module.exports = router;
